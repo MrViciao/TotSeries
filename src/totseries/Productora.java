ed160cccb83a5896adab98801f00d675f6f2745d
Gerard Cervilla <mrviciao@gmail.com>
2016-11-26 20:23:30 +0100

    Añadido resto de clases

---
+++ src/totseries/Productora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package totseries;
+
+import java.util.Date;
+
+/**
+ *
+ * @author mrviciao
+ */
+public class Productora {
+    private String nombre;
+    private int id;
+    private Date f_creacion;
+}
