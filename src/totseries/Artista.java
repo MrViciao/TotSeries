16bbc9e2aab19c86748e0d46b05a0f1ac00d25d9
abeljdrs <abeljdrs@hotmail.com>
2016-11-28 19:57:10 +0100

    Update Artista.java

--- src/totseries/Artista.java
+++ src/totseries/Artista.java
@@ -15,7 +15,33 @@ public abstract class Artista {
     protected String pais;
     
     public Artista(String id, String nombre, String pais){
-        
+        this.id = id;
+        this.nombre = nombre;
+        this.pais = pais;
+    }    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
     }
     
 }
