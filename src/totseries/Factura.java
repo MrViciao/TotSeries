e96e6a4af1b03a11d22f9d3e0b67b798a4f941cf
abeljdrs <abeljdrs@hotmail.com>
2016-11-28 21:18:35 +0100

    Update Factura.java
    utilizo el visualizaciones como contador (podria usar precio) pero si cambian el valor del streaming habria que agregarlo en cambio asi ya seria solo cambiar el multiplicador al momento de generar el precio. se podria usar una variable estatica que sea costo.

--- src/totseries/Factura.java
+++ src/totseries/Factura.java
@@ -14,4 +14,20 @@ import java.util.Date;
 public class Factura {
     private float precio;
     private Date fecha;
+    private int visualizaciones;
+    
+    public Factura(Date fecha){
+        this.precio = 0;
+        this.visualizaciones = 0;
+        this.fecha = fecha;
+    }
+    public float getPrecio() {
+        return precio;
+    }
+    public void setVisualizacion(){
+        this.visualizaciones++;
+    }
+    public void setprecio(){
+        this.precio=this.visualizaciones*1;
+    }
 }
